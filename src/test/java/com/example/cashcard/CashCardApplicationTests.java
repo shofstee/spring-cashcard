c53378cfb17db7117b321a2312973be743efc6bd
Sander Hofstee <sander.hofstee@topicus.nl>
2026-01-27 21:28:52 +0100

    Lab: Implementing GET

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -1,15 +1,44 @@
 package com.example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
-@SpringBootTest
+@AutoConfigureRestTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests
 {
+	@Autowired
+	RestTestClient restTemplate;
 
 	@Test
-	void contextLoads()
+	void shouldReturnACashCardWhenDataIsSaved()
 	{
+		final String expected = """
+			{
+			    "id":99,
+			    "amount":123.45
+			}
+			""";
+		restTemplate.get().uri("/cashcards/99", String.class)
+			.exchange()
+			.expectStatus()
+			.is2xxSuccessful()
+			.expectBody()
+			.json(expected);
+	}
+
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId()
+	{
+		restTemplate.get().uri("/cashcards/100", String.class)
+			.exchange()
+			.expectStatus()
+			.isNotFound()
+			.expectBody()
+			.isEmpty();
 	}
 
 }
